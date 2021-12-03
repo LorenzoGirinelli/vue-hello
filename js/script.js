6d1e46812e479b97769cbe83f68bb6794f7a7522
LorenzoGirinelli <girinellilorenzo22@hotmail.com>
2021-12-03 09:46:20 +0100

    finito

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
+Vue.config.devtools = true;
+
 const app = new Vue(
     {
         el:'#root',
         data: {
-            titleText:'Buonasera'
+            titleText:'Buonasera',
+            imgSrc: 'pexels-bill-emrich-230794-0.jpg'
         }
     }
-)
+);
